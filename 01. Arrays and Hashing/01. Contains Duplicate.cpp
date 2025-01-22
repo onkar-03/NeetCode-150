8f0630793119f1285c8f15358989613a81de6c61
Onkar Patel <onkarpatel10@gmail.com>
2025-01-22 16:01:16 +0530

    Added the Brute Force Approach of it as well

--- 01. Arrays and Hashing/01. Contains Duplicate.cpp
+++ 01. Arrays and Hashing/01. Contains Duplicate.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Appraoch-1: Brute Force
+// T.C : O(n^2) - We use two nested loops to compare each element with every other element, resulting in a quadratic time complexity.
+// S.C : O(1) - The space complexity is constant since we are not using any extra data structures.
+class Solution
+{
+public:
+  bool hasDuplicate(vector<int> &nums)
+  {
+    // Traverse through each element in the array
+    for (int i = 0; i < nums.size(); i++)
+    {
+      // Compare the current element with every subsequent element in the array
+      for (int j = i + 1; j < nums.size(); j++)
+      {
+        // If a duplicate is found, return true
+        if (nums[i] == nums[j])
+        {
+          return true;
+        }
+      }
+    }
+    // If no duplicates are found, return false
+    return false;
+  }
+};
+
 // Approach : Using Hash Map
 // T.C : O(n) - We iterate through the array once, and each map operation (find and insert) takes O(1) on average
 // S.C : O(n) - The unordered_map stores up to n elements in the worst case.
